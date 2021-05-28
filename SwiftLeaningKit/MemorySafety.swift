294a2b676f6204af49894447d9e083d68363204f
fanshuaifei <fanshuaifei@baidu.com>
2021-05-28 20:11:55 +0800

    update

---
+++ SwiftLeaningKit/MemorySafety.swift
@@ -0,0 +1,40 @@
+//
+//  MemorySafety.swift
+//  SwiftLeaningKit
+//
+//  Created by performance on 2021/5/28.
+//  Copyright © 2021 SwiftLeaning. All rights reserved.
+//
+
+import Foundation
+
+
+struct MemorySafety: Runable {
+    static func run() {
+//        struct Student {
+//            var age: Int = 0 {
+//                willSet {}
+//                didSet {}
+//            }
+//            var studentID: Int = 100000
+//        }
+//
+//
+//
+//        var student = Student(age: 10, studentID: 10)
+//        func changeAge(age: inout Int) {
+//            age = 100
+//            student.age = 20
+//        }
+//
+//        func changeStudentID(studentID: inout Int) {
+//            student.studentID = 200000
+//            studentID = 600000
+//        }
+//        崩溃，会有内存安全问题
+//        changeAge(age: &student.age)
+//        changeStudentID(studentID: &student.studentID)
+//        print("student.age:", student.age) // print 100
+//        print("student.studentID:", student.age) // print 200000
+    }
+}
